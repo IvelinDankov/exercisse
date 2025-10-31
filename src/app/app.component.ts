73b11a881251917da9b79a1263ada312f63e8500
Velin Dankov <velin80@web.de>
2025-10-31 09:28:35 +0200

    Form data and form reset.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ViewEncapsulation } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ControlComponent } from "./shared/control/control.component";
+import { ButtonComponent } from "./shared/button/button.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, ControlComponent, ButtonComponent, FormsModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'exercisse';
+  title = "exercisse";
+
+  onSubmit(title: string, text: string, form: HTMLFormElement) {
+    console.log(title);
+    console.log(text);
+
+    form.reset();
+  }
 }
---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-control",
+  imports: [],
+  templateUrl: "./control.component.html",
+  styleUrl: "./control.component.css",
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: "control",
+  },
+})
+export class ControlComponent {
+  label = input.required<string>();
+  for = input.required<string>();
+}
